0c863a909e124fe2666417edaca168da918b6abe
Stefan Kamphausen <stefan.kamphausen@acrolinx.com>
2019-12-13 14:39:10 +0100

    project stub

---
+++ acrolinx-mode.el
@@ -0,0 +1,33 @@
+;;; acrolinx-mode.el --- Check with Acrolinx from within Emacs  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2019 Acrolinx GmbH
+
+;; Authors:
+;; Claus Brunzema <claus.brunzema at acrolinx.com>
+;; Stefan Kamphausen <stefan.kamphausen at acrolinx.com>
+;; Keywords: tools
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; First of all, please note that you need access to Acrolinx which is
+;; a commercial software. Without it, this mode is not for you.
+
+;;; Code:
+
+
+
+(provide 'acrolinx-mode)
+;;; acrolinx-mode.el ends here

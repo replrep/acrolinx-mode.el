7efdfc6019232236e1bcbf7a9f9bb99ab03876ea
Claus Brunzema <Claus.Brunzema@acrolinx.com>
2020-03-24 12:48:35 +0100

    switch to GPLv2

--- acrolinx-mode.el
+++ acrolinx-mode.el
@@ -7,18 +7,22 @@
 ;; Stefan Kamphausen <stefan.kamphausen at acrolinx.com>
 ;; Keywords: tools
 
-;; This program is free software; you can redistribute it and/or modify
-;; it under the terms of the GNU General Public License as published by
-;; the Free Software Foundation, either version 3 of the License, or
-;; (at your option) any later version.
+;; This file is not part of Emacs
 
-;; This program is distributed in the hope that it will be useful,
+;; This file is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 2, or (at your option)
+;; any later version.
+
+;; This file is distributed in the hope that it will be useful,
 ;; but WITHOUT ANY WARRANTY; without even the implied warranty of
 ;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ;; GNU General Public License for more details.
 
 ;; You should have received a copy of the GNU General Public License
-;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+;; along with this program; if not, write to the Free Software
+;; Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+;; 02110-1301 USA or see <http://www.gnu.org/licenses/>.
 
 
 ;;; Commentary:
